6aa6039d04ac8e2d9a2ea9b9e373b83917847b01
Francis M Schappo <francismallmann@gmail.com>
2016-04-26 23:09:53 -0300

    update

--- T6/prog.pl
+++ T6/prog.pl
@@ -53,15 +53,36 @@ zipmult([H|T], [H1|T1], [H2|T2]) :-
 
 
 %Defina um predicado potencias(N,L), de forma que L seja uma lista com as N primeiras potências de 2, sendo a primeira 2^0 e assim por diante, conforme o exemplo abaixo:
+potencias(N, L) :- S is 0, 
+	Aux(S, N, L).
 
+Aux(_, 0 , []).
+Aux(N, M, [H|T]) :- 
+	H is 2^N, 
+	N1 is N+1, 
+	M1 is M-1, 
+	Aux(N1, M1, T).
 
 
 
 %Defina um predicado positivos(L1,L2), de forma que L2 seja uma lista só com os elementos positivos de L1, conforme o exemplo abaixo:
+positivos([], []).
+	positivos([H1|T1], [H2|T2]) :- 
+	H1 > 0, 
+	H2 is H1, 
+	positivos(T1, T2).
+positivos([H1|T1], L) :- 
+	H1 =< 0, 
+	positivos(T1, L).
+
 
 
 
 %Considere que L1 e L2 sejam permutações de uma lista de elementos distintos, sem repetições. Sabendo disso, defina um predicado mesmaPosicao(A,L1,L2) para verificar se um elemento A está na mesma posição nas listas L1 e L2. Exemplo de uso:
+mesmaPosicao(H, [H|_], [H|_]).
+mesmaPosicao(A, [_|T], [_|T1]) :- 
+	mesmaPosicao(A, T, T1).
+
 
 
 
@@ -70,6 +91,11 @@ zipmult([H|T], [H1|T1], [H2|T2]) :-
 
 
 
-%(Adaptado de OBI2006-F1N1) Tem-se N azulejos 10cm x 10cm e, com eles, deve-se montar um conjunto de quadrados de modo a utilizar todos os azulejos dados, sem sobrepô-los. Inicialmente, deve-se montar o maior quadrado possível; então, com os azulejos que sobraram, deve-se montar o maior quadrado possível, e assim sucessivamente. Por exemplo, se forem dados 31 azulejos, o conjunto montado terá 4 quadrados. Para resolver este problema, você deverá definir um predicado azulejos(NA, NQ), de forma que NQ seja o número de quadrados que se deve montar com NA azulejos. Dica: use os predicados sqrt e floor, pré-definidos em Prolog.
 
+%(Adaptado de OBI2006-F1N1) Tem-se N azulejos 10cm x 10cm e, com eles, deve-se montar um conjunto de quadrados de modo a utilizar todos os azulejos dados, sem sobrepô-los. Inicialmente, deve-se montar o maior quadrado possível; então, com os azulejos que sobraram, deve-se montar o maior quadrado possível, e assim sucessivamente. Por exemplo, se forem dados 31 azulejos, o conjunto montado terá 4 quadrados. Para resolver este problema, você deverá definir um predicado azulejos(NA, NQ), de forma que NQ seja o número de quadrados que se deve montar com NA azulejos. Dica: use os predicados sqrt e floor, pré-definidos em Prolog.
+azulejos(0, 0).
+azulejos(NA, NQ) :- 
+	X is floor(sqrt(NA)),
+	N1 is NA - X * X,
+	azulejos(N1, NQ1),	NQ is NQ1 + 1.
 
